3173a3ed561873d5fb55d1edf9e6238fb903578b
maria-dimitrova <mariq@mail.bg>
2016-08-28 13:44:03 +0300

    Add files via upload

---
+++ Homework_Battleship/assets/js/Player.js
@@ -0,0 +1,27 @@
+/**
+ * 
+ */
+
+var Player = (function() {
+    var _instance;
+    function Player() {
+        var _ammo = 20;
+        var _hits = 0;
+        return {
+            getAmmo: function() {
+                return _ammo;
+            },
+            getHits: function() {
+                return _hits;
+            },
+        }
+    }
+    return {
+        getInstance: function() {
+            if (!_instance) {
+                _instance = new Player();
+            }
+            return _instance;
+        }
+    }
+}())
